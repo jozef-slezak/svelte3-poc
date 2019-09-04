499e781e26a320fb025df584b9b248f250c8edd5
Jozef Slezak <jozef.slezak@gmail.com>
2019-09-04 21:29:04 +0200

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Chip, TextField, Switch } from "smelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+  <Switch>aaa</Switch>
+  <TextField></TextField>
+</main>
